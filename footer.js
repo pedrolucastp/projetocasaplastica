6224b00ed7acae190de55184167fe5636b775606
Pedro Lucas <84178507+pedrolucastp@users.noreply.github.com>
2024-03-05 23:17:13 -0300

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
-const headerContent = `
+const footerContent = `
   <p>&copy; 2024 Projeto Casa Plastica. </br> Todos os direitos reservados.</p>
 `
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('footer').innerHTML = headerContent;
+  document.getElementById('footer').innerHTML = footerContent;
 });
