37fd14e01dc3651749baaf8a3d6fe83e71d78e3a
Pedro Lucas <84178507+pedrolucastp@users.noreply.github.com>
2024-03-05 23:45:59 -0300

    Update header.js

--- header.js
+++ header.js
@@ -1,10 +1,10 @@
 const headerContent = `
   <nav>
-    <a href="#" data-page="home"><img src="/logo.png" alt="Casa Plastica" class="logo" height="80" width="197"></a>
+    <a href="/" data-page="home"><img src="/logo.png" alt="Casa Plastica" class="logo" height="80" width="197"></a>
     <ul>
-      <li><a href="#" data-page="sobre">Sobre</a></li>
-      <li><a href="#" data-page="servicos">Serviços</a></li>
-      <li><a href="#" data-page="contato">Contato</a></li>
+      <li><a href="/" data-page="sobre">Sobre</a></li>
+      <li><a href="/" data-page="servicos">Serviços</a></li>
+      <li><a href="/" data-page="contato">Contato</a></li>
     </ul>
   </nav>
 `
