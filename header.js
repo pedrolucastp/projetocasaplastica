7ad60fb6dcdec99d02d82575fc25abeaec8324f7
Pedro Lucas <84178507+pedrolucastp@users.noreply.github.com>
2024-03-05 23:11:59 -0300

    Create header.js

---
+++ header.js
@@ -0,0 +1,14 @@
+const headerContent = `
+  <nav>
+    <a href="#" data-page="home"><img src="/logo.png" alt="Casa Plastica" class="logo"></a>
+    <ul>
+      <li><a href="#" data-page="sobre">Sobre</a></li>
+      <li><a href="#" data-page="servicos">Serviços</a></li>
+      <li><a href="#" data-page="contato">Contato</a></li>
+    </ul>
+  </nav>
+`
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('header').innerHTML = headerContent;
+});
