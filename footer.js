1e53d1a2751a65d02b4859c9d278b76b593d2fd5
Pedro Lucas <84178507+pedrolucastp@users.noreply.github.com>
2024-03-05 23:12:35 -0300

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,7 @@
+const headerContent = `
+  <p>&copy; 2024 Projeto Casa Plastica. </br> Todos os direitos reservados.</p>
+`
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('footer').innerHTML = headerContent;
+});
