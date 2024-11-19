93133dde998b75db5d5d8b994be0d4d74605a39a
pedrolucastp <pedrolucasp@gmail.com>
2024-11-19 01:58:55 -0300

    update page

--- main.js
+++ main.js
@@ -1,48 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Carregar header e footer
-    loadHTML('header', 'header.html');
-    loadHTML('footer', 'footer.html');
+document.addEventListener("DOMContentLoaded", () => {
+  loadHTML("header", "header.html", initializeMenu);
+  loadHTML("footer", "footer.html");
 
-    // Initialize page content
-    document.querySelectorAll('nav a').forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            const page = this.getAttribute('data-page');
-            loadPageContent(page);
-        });
+  function initializeMenu() {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navMenu = document.querySelector(".nav-menu");
+    const body = document.body;
+
+    menuToggle.addEventListener("click", () => {
+      navMenu.classList.toggle("open");
+      body.classList.toggle("menu-open");
     });
 
-    //loadPageContent('home');
+    navMenu.querySelectorAll("a").forEach((link) => {
+      link.addEventListener("click", () => {
+        navMenu.classList.remove("open");
+        body.classList.remove("menu-open");
+      });
+    });
+  }
 
-    // Hide the splash screen after it fades out
-    setTimeout(() => {
-        const splash = document.getElementById('splash-screen');
-        if (splash) {
-            splash.style.display = 'none';
-        }
-    }, 3000); // corresponds to the duration of the fade out animation
-});
-
-// function loadPageContent(pageKey) {
-//     document.querySelectorAll('main section').forEach(section => {
-//         section.classList.add('hidden');
-//     });
-
-//     const selectedSection = document.getElementById(pageKey);
-//     if (selectedSection) {
-//         selectedSection.classList.remove('hidden');
-//     } else {
-//         console.error(`No content found for page: ${pageKey}`);
-//     }
-// }
-
-// Função para carregar header e footer
-function loadHTML(elementID, filePath) {
+  function loadHTML(elementID, filePath, callback) {
     const element = document.getElementById(elementID);
     fetch(filePath)
-        .then(response => response.text())
-        .then(data => {
-            element.innerHTML = data;
-        })
-        .catch(error => console.error('Erro ao carregar o arquivo:', error));
-}
+      .then((response) => response.text())
+      .then((data) => {
+        element.innerHTML = data;
+        if (callback) callback(); // Execute the callback after content is loaded
+      })
+      .catch((error) => console.error("Erro ao carregar o arquivo:", error));
+  }
+});
